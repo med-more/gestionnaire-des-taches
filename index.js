f0de8baf833509a10fbebae042ed131eb2bf6e06
Mohammed Baba <mbfmz20@gmail.com>
2024-12-27 12:14:29 +0100

    config de app et lancement du server

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const bodyParser = require('body-parser');//impor de middleware
 const taskRoutes = require('./routes/tasks');//import des routs
 const connectDB = require('./db');//etablir la connection a la base de donneé
 
+const app = express();//configurer l'application Express
+const PORT = 5000;
+
+app.use(bodyParser.json());//configure un middleware Express pour analyser les données JSON
+connectDB();//connexion a mongodb
+
+app.use('/tasks', taskRoutes);//configure un middleware pour gérer toutes les requêtes HTTP
+
+// Lancement le serveur
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+  });
