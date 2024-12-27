54dc6df9ba3b9d0160b73b1d4010f9f0ddf09479
Mohammed Baba <mbfmz20@gmail.com>
2024-12-27 15:12:50 +0100

    ajout de fonction d'ajout d'une tache

---
+++ routes/tasks.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Task = require('../models/task');
+
+const router = express.Router();//creer nouvelle objet
+
+// Ajouter une tâche
+router.post('/', async (req, res) => {
+  try {
+    const { title, description } = req.body;//envoyer les information du corps de la requetta
+    if (!title || !description) {
+      return res.status(400).json({ message: 'Le titre et la description obligatoires.'});
+    }
+    const newTask = new Task({ title, description });//creation d'une nouvelle instance
+    await newTask.save();//enregistrer une nouveele tache dans mongodb
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(500).json({ message: 'Erreur serveur', error: err.message });
+  }
+});
+
