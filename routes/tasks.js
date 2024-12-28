ba8a8a70e9b622517137433abedfdac0877ee517
Mohammed Baba <mbfmz20@gmail.com>
2024-12-28 10:53:09 +0100

    ajout de fonction pour l'affichage des taches

--- routes/tasks.js
+++ routes/tasks.js
@@ -18,3 +18,15 @@ router.post('/', async (req, res) => {
   }
 });
 
+//afficher tous les taches 
+router.get('/', async (req, res) => {
+    try {
+      const allTasks = await Task.find();
+      res.status(200).json(allTasks);
+    } catch (err) {
+      res.status(500).json({ message: 'Erreur serveur', error: err.message });
+    }
+  });
+  
+
+module.exports = router;
